54c7e74a8e26844185cdf6f3e6bce66cc7d6793e
dvory <kl5837789@gmail.com>
2025-03-18 12:18:53 +0200

    connect to db6

--- Program.cs
+++ Program.cs
@@ -1,3 +1,80 @@
+// using Microsoft.AspNetCore.Mvc;
+// using Microsoft.EntityFrameworkCore;
+// using Microsoft.OpenApi.Models;
+// using TodoApi;
+
+// var builder = WebApplication.CreateBuilder(args);
+
+// builder.Services.AddControllers();
+// builder.Services.AddEndpointsApiExplorer();
+// builder.Services.AddSwaggerGen(c=>{
+// c.SwaggerDoc("v1",new OpenApiInfo{
+//     Version="v1"
+// });
+// });
+
+// builder.Services.AddDbContext<ToDoDbContext>(options =>
+//     options.UseMySql(builder.Configuration.GetConnectionString("ToDoDB"), new MySqlServerVersion(new Version(8, 0, 41))));
+
+// builder.Services.AddCors(option => option.AddPolicy("AllowAll",
+//     p => p.AllowAnyOrigin()
+//     .AllowAnyMethod()
+//     .AllowAnyHeader()));
+
+// var app = builder.Build();
+
+// app.UseCors("AllowAll");
+
+// app.UseSwagger();
+//     app.UseSwagger();
+//    if (builder.Environment.IsDevelopment())
+// { 
+//     app.UseSwaggerUI(options => // UseSwaggerUI is called only in Development.
+//     {
+//         options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+//          options.RoutePrefix = string.Empty;
+//     });
+// }
+
+// app.MapGet("/items", async (ToDoDbContext db) => await db.Items.ToListAsync());
+
+// app.MapPost("/", async (ToDoDbContext db, string name) =>
+// {
+//     Item item = new Item();
+//     item.Name = name;
+//     item.Iscomplete=false;
+
+//     await db.Items.AddAsync(item);
+//     return await db.SaveChangesAsync();
+// });
+
+// app.MapDelete("{id}", async (ToDoDbContext db, int id) =>
+// {
+//     var item = await db.Items.FindAsync(id);
+//     if (item == null) return Results.NotFound();
+//     db.Items.Remove(item);
+//     await db.SaveChangesAsync();
+//     return Results.Ok();
+// });
+
+// app.MapPatch("{id}", async (ToDoDbContext db, int id,bool IsComplete) =>
+// {
+   
+//     var itemToUpdate = await db.Items.FindAsync(id);
+//     if (itemToUpdate == null) return Results.NotFound();
+//     itemToUpdate.Iscomplete = IsComplete;
+//     await db.SaveChangesAsync();
+//     return Results.Ok();
+// });
+
+// app.MapGet("/",()=>"my server is running!!!");
+// app.Run();
+
+
+
+
+
+
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.OpenApi.Models;
@@ -7,10 +84,10 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen(c=>{
-c.SwaggerDoc("v1",new OpenApiInfo{
-    Version="v1"
-});
+builder.Services.AddSwaggerGen(c => {
+    c.SwaggerDoc("v1", new OpenApiInfo {
+        Version = "v1"
+    });
 });
 
 builder.Services.AddDbContext<ToDoDbContext>(options =>
@@ -18,38 +95,37 @@ builder.Services.AddDbContext<ToDoDbContext>(options =>
 
 builder.Services.AddCors(option => option.AddPolicy("AllowAll",
     p => p.AllowAnyOrigin()
-    .AllowAnyMethod()
-    .AllowAnyHeader()));
+          .AllowAnyMethod()
+          .AllowAnyHeader()));
 
 var app = builder.Build();
 
 app.UseCors("AllowAll");
 
 app.UseSwagger();
-    app.UseSwagger();
-   if (builder.Environment.IsDevelopment())
-{ 
-    app.UseSwaggerUI(options => // UseSwaggerUI is called only in Development.
-    {
+if (builder.Environment.IsDevelopment()) {
+    app.UseSwaggerUI(options => {
         options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
-         options.RoutePrefix = string.Empty;
+        options.RoutePrefix = string.Empty;
     });
 }
 
+// מסלול GET להבאת כל הפריטים
 app.MapGet("/items", async (ToDoDbContext db) => await db.Items.ToListAsync());
 
-app.MapPost("/", async (ToDoDbContext db, string name) =>
-{
+// מסלול POST להוספת פריט חדש
+app.MapPost("/", async (ToDoDbContext db, string name) => {
     Item item = new Item();
     item.Name = name;
-    item.Iscomplete=false;
+    item.Iscomplete = false;
 
     await db.Items.AddAsync(item);
-    return await db.SaveChangesAsync();
+    await db.SaveChangesAsync();
+    return Results.Ok(item);
 });
 
-app.MapDelete("{id}", async (ToDoDbContext db, int id) =>
-{
+// מסלול DELETE למחיקת פריט לפי מזהה
+app.MapDelete("{id}", async (ToDoDbContext db, int id) => {
     var item = await db.Items.FindAsync(id);
     if (item == null) return Results.NotFound();
     db.Items.Remove(item);
@@ -57,9 +133,8 @@ app.MapDelete("{id}", async (ToDoDbContext db, int id) =>
     return Results.Ok();
 });
 
-app.MapPatch("{id}", async (ToDoDbContext db, int id,bool IsComplete) =>
-{
-   
+// מסלול PATCH לעדכון פריט לפי מזהה
+app.MapPatch("{id}", async (ToDoDbContext db, int id, bool IsComplete) => {
     var itemToUpdate = await db.Items.FindAsync(id);
     if (itemToUpdate == null) return Results.NotFound();
     itemToUpdate.Iscomplete = IsComplete;
@@ -67,5 +142,7 @@ app.MapPatch("{id}", async (ToDoDbContext db, int id,bool IsComplete) =>
     return Results.Ok();
 });
 
-app.MapGet("/",()=>"my server is running!!!");
+// מסלול GET לבדיקת מצב השרת
+app.MapGet("/", () => "my server is running!!!");
+
 app.Run();
